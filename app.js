ea057f1cbe2cc6720975418272eb19ebac6777e2
waleed_hammad <waleedhammadmohammed@gmail.com>
2026-04-02 19:13:20 +0200

    Add error handling middleware: implement centralized error handling for various error types in the application

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 import express from 'express';
-
 import { NODE_ENV, PORT } from './config/env.js';
-
-const app = express();
-
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/users',userRouter)
 app.use('/api/v1/subscriptions',subscriptionRouter)
 
-
+app.use(errorMiddleware)
 app.listen(PORT, async() => {
     console.log(`Server started on http://localhost:${PORT}`);
     console.log(`connected to database in ${NODE_ENV} mode`)
